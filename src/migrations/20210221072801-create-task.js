64ddc90d5da3f65b3de9a682cf773b69cd8219b1
nagamoridaiki <no.swea1017@gmail.com>
2021-02-21 16:54:22 +0900

    dockerがおかしくなったのでプロジェクトを作り直した

--- src/migrations/20210221072801-create-task.js
+++ src/migrations/20210221072801-create-task.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Tasks', {
+    await queryInterface.createTable('Task', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Tasks');
+    await queryInterface.dropTable('Task');
   }
 };
