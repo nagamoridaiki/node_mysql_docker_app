d2a44376814a7a5893523a99fddb0c8ab19ae69a
nagamoridaiki <no.swea1017@gmail.com>
2021-03-03 21:55:57 +0900

    いいね情報の作成ができるまで

---
+++ src/models/like.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Like extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    Like.init({
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "userId"
+            }
+        },
+        boardId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "board",
+                key: "boardId"
+            }
+        }
+    }, {
+        sequelize,
+        modelName: 'Like',
+    });
+    return Like;
+};
