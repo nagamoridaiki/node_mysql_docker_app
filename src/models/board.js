357b85744e9f78747b73696dae8387bebaeca9f5
nagamoridaiki <no.swea1017@gmail.com>
2021-03-03 21:32:35 +0900

    いいねテーブルにいいねデータを作成ができた

--- src/models/board.js
+++ src/models/board.js
@@ -1,19 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Board extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Board.init({
+  const Board = sequelize.define('Board', {
     userId: {
       type: DataTypes.INTEGER,
       validate: {
@@ -23,38 +10,32 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     title: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          msg: "タイトルは必須です。"
-        },
-        len: {
-          args: [1, 50],
-          msg: "1〜50字以内で入力してください"
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: "タイトルは必須です。"
+          },
+          len: {
+            args: [1, 50],
+            msg: "1〜50字以内で入力してください"
+          }
+        }
+      },
+      message: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: "本文は必須です。"
+          },
+          len: {
+            args: [1, 140],
+            msg: "1〜140字以内で入力してください"
+          }
         }
       }
-    },
-    message: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          msg: "本文は必須です。"
-        },
-        len: {
-          args: [1, 140],
-          msg: "1〜140字以内で入力してください"
-        }
-      }
-    }
-  }, {
-    sequelize,
-    modelName: 'Board',
-  });
+  }, {});
   Board.associate = function(models) {
-    Board.belongsTo(models.User, {
-      foreignKey: 'userId',
-      sourceKey: 'id'
-    });
+    Board.belongsTo(models.User);
   };
   return Board;
 };
